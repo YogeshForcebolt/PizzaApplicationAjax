78b85a470eaa0386bc1391c611cb649d8ae28222
YogeshForcebolt <yogeshkumar@forcebolt.com>
2022-12-14 19:24:08 +0530

    initial commit

---
+++ PizzaProject/Models/DataBaseServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaProject.Models
+{
+    public class DataBaseServices
+    {
+        public Dictionary<string, int> pizzaSize = new Dictionary<string, int>();
+        public Dictionary<string, int> pizzaTopping = new Dictionary<string, int>();
+
+        public DataBaseServices() {
+            pizzaSize["Small"] = 80;
+            pizzaSize["Medium"] = 120;
+            pizzaSize["Large"] = 150;
+            pizzaTopping["Extra Cheese"] = 40;
+            pizzaTopping["Pepporni"] = 80;
+            pizzaTopping["Artichoke"] = 60;
+            pizzaTopping["Anchovy"] = 120;
+        }
+    }
+}
